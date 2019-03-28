d99b440426757ab8ddeb3053532e8a899079944a
HariharanUK <hariharandhanpaluk@gmail.com>
2019-03-28 18:20:55 +0530

    added maximum salary

--- src/main/java/com/booklib/CompartorLambdas.java
+++ src/main/java/com/booklib/CompartorLambdas.java
@@ -8,11 +8,11 @@ public class CompartorLambdas {
 
 	public static void main(String[] args) {
 		List<Employee> ls = new ArrayList<>();
-		ls.add(Employee.builder().id("1").name("Zebra").role("Tester").salary("100").build());
-		ls.add(Employee.builder().id("4").name("Bison").role("Devloper").salary("100").build());
-		ls.add(Employee.builder().id("5").name("Lion").role("Client").salary("100").build());
-		ls.add(Employee.builder().id("3").name("Tiger").role("Network").salary("100").build());
-		ls.add(Employee.builder().id("2").name("Samba").role("Junior").salary("100").build());
+		ls.add(Employee.builder().id("1").name("Zebra").role("Tester").salary("300").hike(3300).build());
+		ls.add(Employee.builder().id("4").name("Bison").role("Devloper").salary("400").hike(500).build());
+		ls.add(Employee.builder().id("5").name("Lion").role("Client").salary("3400").hike(5500).build());
+		ls.add(Employee.builder().id("3").name("Tiger").role("Network").salary("5400").hike(4300).build());
+		ls.add(Employee.builder().id("2").name("Samba").role("Junior").salary("100").hike(3300).build());
 		
 		//B4 J8
 		System.out.println("----------------B4-J8--------------------");
@@ -32,6 +32,7 @@ public class CompartorLambdas {
 		
 		ls.sort((Employee e1, Employee e2) -> {return e1.getName().compareTo(e2.getName());});
 		
+		
 		ls.forEach(System.out::println);
 		
 
@@ -50,5 +51,12 @@ public class CompartorLambdas {
 		ls.sort((Employee e1, Employee e2) -> {return e1.getRole().compareTo(e2.getRole());});
 		
 		ls.forEach(System.out::println);
+		
+		// Get 3 maximum salary of employee
+		
+		ls.stream().sorted(Comparator.comparingInt(Employee::getHike).reversed()).limit(3).map(Employee::getName).forEach(System.out::println);
+		
+				
+		
 	}
 }
